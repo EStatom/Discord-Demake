e0f25ad6a1caaf4e5139eef5e480f2ad06e546ce
Khan Raqib Mahmud <161790849+khanRmahmud@users.noreply.github.com>
2024-10-10 08:56:32 -0500

    this is the example main page
    when click on close button of account info, it redirect to this main page from account info page.

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <h1>Wellcome to Discord.</h1>
+      <p>This is the main page of Discord...</p>
+    </div>
+  );
+};
+
+export default MainPage;
