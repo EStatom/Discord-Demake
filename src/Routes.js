6338af968e05248b69738eca0de52794949f5666
EStatom <106710797+EStatom@users.noreply.github.com>
2024-10-09 22:01:41 -0500

    Added Account Info PAges

--- src/Routes.js
+++ src/Routes.js
@@ -7,11 +7,46 @@ import Login from './pages/components/Login';
 import SignUp from './pages/components/SignUp';
 import ForgotPassword from './pages/components/ForgotPassword';
 import ChatApp from './pages/components/ChatApp';
+import AccountInfo from './pages/components/AccountInfo';
+import ProfileEdit from './pages/components/ProfileEdit';
 import {doc, getDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
+import bannerImage from './pages/images/image-1.jpg';
+import profileImage from './pages/images/profile-1.jpeg';
+
 function App() {
     const [serverDetails, setServerDetails] = useState(null);
+
+    const [profileData, setProfileData] = useState({
+        displayName: 'Khan Mahmud',
+        username: 'khan_latech',
+        email: '******@email.latech.edu',
+        phoneNumber: '******5630',
+        pronouns: '',
+        avatar: profileImage,
+        banner: bannerImage,
+        joinedDate: 'January 1, 2020',
+        friendsList: [
+          {
+            name: 'Friend 1',
+            avatar: './images/friend1.jpg',
+            status: 'online',
+          },
+          {
+            name: 'Friend 2',
+            avatar: './images/friend2.jpg',
+            status: 'offline',
+          },
+        ],
+      });
+
+    const handleProfileChange = (updatedData) => {
+        setProfileData({
+            ...profileData,
+            ...updatedData,
+        });
+    };
 
     // Use this to help or url processing, its an extension of our Routing System
     const navigate = useNavigate();
@@ -40,10 +75,12 @@ function App() {
         fetchServerDetails(serverId);
     };
 
+    
+
   return (
         <div style={{display: 'flex', width: '100dvw'}}>
             <Sidebar onSelectServer={handleSelectedServer} />
-            <div className="server-content" style={{width: '100%'}}>
+            <div className="server-content" style={{width: '100%', minHeight: '100dvh'}}>
                 <Routes>
                     {/* Route for displaying server details; ':serverId' tells the server this is a variable */}
                     <Route path="/server/:serverId" element={<ChatApp serverDetails={serverDetails}/>}/>
@@ -54,6 +91,9 @@ function App() {
                     <Route path='/Signup' element={<SignUp/>}/>
                     <Route path='/ForgotPassword' element={<ForgotPassword/>}/>
                     
+                    <Route path='/AccountInfo' element={<AccountInfo profileData={profileData} />}/>
+                    <Route path='/ProfileEdit' element={<ProfileEdit profileData={profileData} onProfileChange={handleProfileChange} />}/>
+
                 </Routes>
             </div>
         </div>
